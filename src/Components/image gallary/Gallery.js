ed2b5b1e3e9b5dd2278b78927ade5c816b84140e
getch-hasan <hasan15-3112@diu.edu.bd>
2023-11-02 02:56:05 +0600

    after move image by index

--- src/Components/image gallary/Gallery.js
+++ src/Components/image gallary/Gallery.js
@@ -1,50 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import image1 from '../../assets/images/image-1.webp'
-import image2 from '../../assets/images/image-2.webp'
-import image3 from '../../assets/images/image-3.webp'
-import image4 from '../../assets/images/image-4.webp'
-import image5 from '../../assets/images/image-5.webp'
-import image6 from '../../assets/images/image-6.webp'
-import image7 from '../../assets/images/image-7.webp'
-import image8 from '../../assets/images/image-8.webp'
+import React, { useState } from 'react';
+import image1 from '../../assets/images/image-1.webp';
+import image2 from '../../assets/images/image-2.webp';
+import image3 from '../../assets/images/image-3.webp';
+import image4 from '../../assets/images/image-4.webp';
+import image5 from '../../assets/images/image-5.webp';
+import image6 from '../../assets/images/image-6.webp';
+import image7 from '../../assets/images/image-7.webp';
+import image8 from '../../assets/images/image-8.webp';
 import Images from './Images';
-import './Gallery.css'
-const Gallery = () => {
-    const [images, setImages] = useState([{
-        id: 1, img: image1
-    },
-    {
-        id: 2, img: image2
-    },
-    {
-        id: 3, img: image3
-    },
-    {
-        id: 4, img: image4
-    },
-    {
-        id: 5, img: image5
-    },
-    {
-        id: 6, img: image6
-    },
-    {
-        id: 7, img: image7
-    },
-    {
-        id: 8, img: image8
-    },])
+import imgIcon from '../../assets/images/image-solid.svg';
+import './Gallery.css';
+import { DndProvider, useDrag, useDrop } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
-    console.log(images)
-    return (
-        <div className='grid grid-cols-5 gap-4 m-5'>
-            {
-                images.map((image, index) => <Images image={image}
-                index={index}
-                    ></Images>)
-            }
+const Gallery = () => {
+  const [images, setImages] = useState([
+    { id: 1, img: image1 },
+    { id: 2, img: image2 },
+    { id: 3, img: image3 },
+    { id: 4, img: image4 },
+    { id: 5, img: image5 },
+    { id: 6, img: image6 },
+    { id: 7, img: image7 },
+    { id: 8, img: image8 },
+  ]);
+
+  const moveImage = (fromIndex, toIndex) => {
+    const updatedImages = [...images];
+    const [movedImage] = updatedImages.splice(fromIndex, 1);
+    updatedImages.splice(toIndex, 0, movedImage);
+    setImages(updatedImages);
+  };
+
+  const renderImages = images.map((image, index) => (
+    <Images
+      key={image.id}
+      image={image}
+      index={index}
+      moveImage={moveImage}
+    />
+  ));
+
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div className='grid grid-cols-5 gap-4 m-5'>
+        {renderImages}
+        <div
+          className='border-dashed border-2 border-blue-500 rounded-lg grid grid-cols-1 gap-5 content-center items-center'
+        >
+          <img className='h-5 w-5' src={imgIcon} alt='' />
+          <p>Upload your image</p>
         </div>
-    );
+      </div>
+    </DndProvider>
+  );
 };
 
 export default Gallery;
--- src/Components/image gallary/Images.js
+++ src/Components/image gallary/Images.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import { useDrag, useDrop } from 'react-dnd';
 
-const Images = ({ image, index }) => {
-    return (
-        <div className={`${
-            index === 0 ? 'col-span-2 row-span-2 border-2 hover:bg-slate-400 border-black rounded-lg' 
-            : 
-            'col-span-1 static hover:bg-slate-400 row-span-1 border-2 border-blue-500 rounded-s-lg'
-          }` } >
-            <label className='relative'>
-                <input className='absolute m-4' type="checkbox" name="" id="" />
-                <img className='hover:opacity-75' src={image?.img} alt="" />
-            </label>
-            
-        </div>
-    );
+const Images = ({ image, index, moveImage }) => {
+  const [, ref] = useDrag({
+    type: 'IMAGE',
+    item: { index },
+  });
+
+  const [, drop] = useDrop({
+    accept: 'IMAGE',
+    hover: (draggedItem) => {
+      if (draggedItem.index !== index) {
+        moveImage(draggedItem.index, index);
+        draggedItem.index = index;
+      }
+    },
+  });
+
+  return (
+    <div
+      ref={(node) => {
+        ref(drop(node));
+      }}
+      className={`${
+        index === 0
+          ? 'col-span-2 row-span-2 border-2 hover:bg-slate-400 border-black rounded-lg'
+          : 'col-span-1 static hover:bg-slate-400 row-span-1 border-2 border-blue-500 rounded-s-lg'
+      }`}
+    >
+      <label className='relative'>
+        <input className='absolute m-4' type='checkbox' name='' id='' />
+        <img className='hover:opacity-75' src={image?.img} alt='' />
+      </label>
+    </div>
+  );
 };
 
 export default Images;
